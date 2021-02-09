a1253313e5b7b124b89d55ace15de12d97c80088
Jady Hom <hyjady@gmail.com>
2021-02-09 16:57:15 -0500

    imported components into App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,22 @@
 import "./styles/App.css";
+import Navbar from "../src/components/Navbar";
+import Header from "../src/components/Header";
+import Work from "../src/components/Work";
+import Skill from "../src/components/Skill";
+import About from "../src/components/About";
+import Contact from "../src/components/Contact";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <Router />
+      <Navbar />
+      <Header />
+      <Work />
+      <Skill />
+      <About />
+      <Contact />
     </div>
   );
-}
+};
 
 export default App;
