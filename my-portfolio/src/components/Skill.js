7c2fc1b5067e8f98150c0926801a517498447bc1
Jady Hom <hyjady@gmail.com>
2021-02-09 16:56:49 -0500

    setup skill section

---
+++ my-portfolio/src/components/Skill.js
@@ -0,0 +1,85 @@
+import React from "react";
+
+const Skill = () => {
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+      <div className="skills-container">
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612898516/portfolio/skill-icons/html_azx9cg.png"
+            alt="html-icon"
+            className="skill-img"
+          />
+          <p>HTML5</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612898516/portfolio/skill-icons/css_ldayvi.png"
+            alt="css-icon"
+            className="skill-img"
+          />
+          <p>CSS3</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/js_vas8kd.png"
+            alt="javascript-icon"
+            className="skill-img"
+          />
+          <p>JavaScript</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/express_afuilu.png"
+            alt="express-icon"
+            className="skill-img"
+          />
+          <p>Express</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/react_m2cutu.png"
+            alt="react-icon"
+            className="skill-img"
+          />
+          <p>React</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/nodejs_ocbmxn.png"
+            alt="node-icon"
+            className="skill-img"
+          />
+          <p>Node.js</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/mongodb_uofowj.png"
+            alt="mongodb-icon"
+            className="skill-img"
+          />
+          <p>MongoDB</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/postgresql_nxhf2y.png"
+            alt="postgresql-icon"
+            className="skill-img"
+          />
+          <p>PostgreSQL</p>
+        </div>
+        <div className="skill">
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1612897702/portfolio/skill-icons/python_elt9hz.png"
+            alt="python-icon"
+            className="skill-img"
+          />
+          <p>Python</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
