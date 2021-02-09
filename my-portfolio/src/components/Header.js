5f7271122daf14eb8e0dd2b0ca8e2c393f5ec453
Jady Hom <hyjady@gmail.com>
2021-02-09 16:56:32 -0500

    setup header

---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="top">
+      <div className="header">
+        <h1 className="description">
+        Hello!<br/> I am a <br /> Software Engineer + <br /> Visual Thinker + <br /> Illustrator
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
