4f4e0bd3757574371681caeccd32addf4aafee72
Jady Hom <hyjady@gmail.com>
2021-02-12 17:24:53 -0500

    setup 2 nav-links

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+// import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  return <div className="name-logo">JADY HOM</div>;
+  return (
+    <div className="nav">
+      <div className="name-logo">JADY HOM</div>
+      <div className="nav-links">Works</div>
+      <div className="nav-links">Illustrations</div>
+    </div>
+  );
 };
 
 export default Navbar;
