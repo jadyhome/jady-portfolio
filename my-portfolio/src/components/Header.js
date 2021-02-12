72ce148c30c9ce3ab744aa00617b8455aa277fd4
Jady Hom <hyjady@gmail.com>
2021-02-12 17:25:13 -0500

    added img to header section

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -4,9 +4,18 @@ const Header = () => {
   return (
     <div className="top">
       <div className="header">
+        <div className="header-container">
         <h1 className="description">
-        Hello!<br/> I am a <br /> Software Engineer + <br /> Visual Thinker + <br /> Illustrator
+          Hello!
+          <br /> I am a <br /> Software Engineer + <br /> Visual Thinker +{" "}
+          <br /> Illustrator
         </h1>
+          <img
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1613165559/portfolio/skill-icons/flowers_mk3j0k.png"
+            alt="flower-bg"
+            className="flowers"
+          />
+        </div>
       </div>
     </div>
   );
