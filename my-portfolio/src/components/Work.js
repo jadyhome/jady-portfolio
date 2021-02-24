f188fccf5d53620b7be187930cf7b87b99259d01
Jady Hom <hyjady@gmail.com>
2021-02-24 18:34:48 -0500

    re-arranged works divs, commented out one work for now

--- my-portfolio/src/components/Work.js
+++ my-portfolio/src/components/Work.js
@@ -3,10 +3,28 @@ import React from "react";
 const Work = () => {
   return (
     <div className="work">
-      <h1>Work</h1>
+      <h1>Works</h1>
       <div className="cards-container">
-        <div className="work1">
-          <h2>blooming</h2>
+        {/* <div className="work1">
+          <div className="work-top">
+            <h2>blooming</h2>
+            <p>
+              <a href="https://github.com/jadyhome/blooming">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
+                  alt="github-icon"
+                  className="github-icon"
+                />
+              </a>
+              <a href="http://blooming.surge.sh/">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
+                  alt="deployed-icon"
+                  className="deployed-icon"
+                />
+              </a>
+            </p>
+          </div>
           <div className="image">
             <img
               src="https://res.cloudinary.com/jadyhome/image/upload/v1612896668/portfolio/blooming.png"
@@ -14,26 +32,28 @@ const Work = () => {
               className="work-image"
             />
           </div>
-          <p>
-            <a href="https://github.com/jadyhome/blooming">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
-                alt="github-icon"
-                className="github-icon"
-              />
-            </a>
-            <a href="http://blooming.surge.sh/">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
-                alt="deployed-icon"
-                className="deployed-icon"
-              />
-            </a>
-          </p>
-        </div>
+        </div> */}
 
         <div className="work2">
-          <h2>arting</h2>
+          <div className="work-top">
+            <h2>arting</h2>
+            <p>
+              <a href="https://github.com/jadyhome/arting">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
+                  alt="github-icon"
+                  className="github-icon"
+                />
+              </a>
+              <a href="https://arting.herokuapp.com/">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
+                  alt="deployed-icon"
+                  className="deployed-icon"
+                />
+              </a>
+            </p>
+          </div>
           <div className="image">
             <img
               src="https://res.cloudinary.com/jadyhome/image/upload/v1612896410/portfolio/arting-page.png"
@@ -41,26 +61,28 @@ const Work = () => {
               className="work-image"
             />
           </div>
-          <p>
-            <a href="https://github.com/jadyhome/arting">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
-                alt="github-icon"
-                className="github-icon"
-              />
-            </a>
-            <a href="https://arting.herokuapp.com/">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
-                alt="deployed-icon"
-                className="deployed-icon"
-              />
-            </a>
-          </p>
         </div>
 
         <div className="work3">
-          <h2>petsagram</h2>
+          <div className="work-top">
+            <h2>petsagram</h2>
+            <p>
+              <a href="https://github.com/davey4/petsagram">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
+                  alt="github-icon"
+                  className="github-icon"
+                />
+              </a>
+              <a href="https://petsagram-105.herokuapp.com/">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
+                  alt="deployed-icon"
+                  className="deployed-icon"
+                />
+              </a>
+            </p>
+          </div>
           <div className="image">
             <img
               src="https://res.cloudinary.com/jadyhome/image/upload/v1612896333/portfolio/petsagram-page.png"
@@ -68,26 +90,28 @@ const Work = () => {
               className="work-image"
             />
           </div>
-          <p>
-            <a href="https://github.com/davey4/petsagram">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
-                alt="github-icon"
-                className="github-icon"
-              />
-            </a>
-            <a href="https://petsagram-105.herokuapp.com/">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
-                alt="deployed-icon"
-                className="deployed-icon"
-              />
-            </a>
-          </p>
         </div>
 
         <div className="work4">
-          <h2>mother-tongue</h2>
+          <div className="work-top">
+            <h2>mother-tongue</h2>
+            <p>
+              <a href="https://github.com/jadyhome/mother-tongue">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
+                  alt="github-icon"
+                  className="github-icon"
+                />
+              </a>
+              <a href="https://mother-tongue.herokuapp.com/">
+                <img
+                  src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
+                  alt="deployed-icon"
+                  className="deployed-icon"
+                />
+              </a>
+            </p>
+          </div>
           <div className="image">
             <img
               src="https://res.cloudinary.com/jadyhome/image/upload/v1612896746/portfolio/mother-tongue.png"
@@ -95,22 +119,6 @@ const Work = () => {
               className="work-image"
             />
           </div>
-          <p>
-            <a href="https://github.com/jadyhome/mother-tongue">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1612906936/portfolio/skill-icons/github.png"
-                alt="github-icon"
-                className="github-icon"
-              />
-            </a>
-            <a href="https://mother-tongue.herokuapp.com/">
-              <img
-                src="https://res.cloudinary.com/jadyhome/image/upload/v1613167890/portfolio/skill-icons/deployed-icon.png"
-                alt="deployed-icon"
-                className="deployed-icon"
-              />
-            </a>
-          </p>
         </div>
       </div>
     </div>
