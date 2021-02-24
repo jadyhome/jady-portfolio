0c1e8f78844a87c05a20286be77484e1f8a72b71
Jady Hom <hyjady@gmail.com>
2021-02-24 18:33:22 -0500

    changed background img and description

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -6,12 +6,11 @@ const Header = () => {
       <div className="header">
         <div className="header-container">
         <h1 className="description">
-          Hello!
-          <br /> I am a <br /> Software Engineer + <br /> Visual Thinker +{" "}
+          Hello, <br /> I am a<br /> Software Engineer + <br /> Visual Thinker +
           <br /> Illustrator
         </h1>
           <img
-            src="https://res.cloudinary.com/jadyhome/image/upload/v1613165559/portfolio/skill-icons/flowers_mk3j0k.png"
+            src="https://res.cloudinary.com/jadyhome/image/upload/v1614201614/portfolio/flowers3.png"
             alt="flower-bg"
             className="flowers"
           />
