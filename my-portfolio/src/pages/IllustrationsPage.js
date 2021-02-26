5dd2f0498fdf5347a2d46c03396f743e0e951bcf
Jady Hom <hyjady@gmail.com>
2021-02-26 17:10:44 -0500

    setup IllustrationsPage

---
+++ my-portfolio/src/pages/IllustrationsPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const IllustrationsPage = () => {
+  return (
+    <div className="illustration">
+      <Navbar />
+      <h1 className="wip">UNDER CONSTRUCTION</h1>
+    </div>
+  );
+};
+
+export default IllustrationsPage;
