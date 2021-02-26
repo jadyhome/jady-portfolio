0d1fcd903c4999049ee94b4dc8abeba7e27dbc43
Jady Hom <hyjady@gmail.com>
2021-02-26 17:09:33 -0500

    added classnames & organized code

--- my-portfolio/src/components/Header.js
+++ my-portfolio/src/components/Header.js
@@ -1,14 +1,19 @@
 import React from "react";
+import "../styles/Header.css"
 
 const Header = () => {
   return (
     <div className="top">
       <div className="header">
         <div className="header-container">
-        <h1 className="description">
-          Hello, <br /> I am a<br /> Software Engineer + <br /> Visual Thinker +
-          <br /> Illustrator
-        </h1>
+          <div className="description">
+            <p className="heading1">Hello, I am a</p>
+            <h1 className="heading2">
+              Software Engineer + <br />
+              Visual Thinker + <br />
+              Illustrator
+            </h1>
+          </div>
           <img
             src="https://res.cloudinary.com/jadyhome/image/upload/v1614201614/portfolio/flowers3.png"
             alt="flower-bg"
