d997b819b4dc61885b80000e15488e67a309b0e6
Jady Hom <hyjady@gmail.com>
2021-07-20 17:11:06 -0400

    imported illustrations to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
-import Work from '../components/Work'
-import About from '../components/About'
+import Work from "../components/Work";
+import About from "../components/About";
+import Illustrations from "../components/Illustrations";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Navbar />
       <Header />
       <Work />
+      <Illustrations />
       <About />
     </div>
   );
