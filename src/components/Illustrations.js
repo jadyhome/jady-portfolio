0ed20c12146e3fd949968c390e61041b0f910980
Jady Hom <hyjady@gmail.com>
2021-07-20 17:10:56 -0400

    starting on illustrations section

---
+++ src/components/Illustrations.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/Illustrations.css";
+
+const Illustrations = () => {
+  return (
+    <div className="illustration">
+      <h1 className="section-title">Illustrations</h1>
+      {/* <div className="cards-container">
+        <div className="illus">
+          <div className="illus-top">
+            <h2>flower series</h2>
+          </div>
+          <div className="image">
+            <img
+              src="https://res.cloudinary.com/jadyhome/image/upload/v1612896410/portfolio/arting-page.png"
+              alt="flower-series"
+              className="work-image"
+            />
+          </div>
+          <h4 className="techs">
+            technologies ↴
+            <br /> HTML5 + CSS3 + JavaScript + React
+            <br /> Node.js + Express + MongoDB + Mongoose
+          </h4>
+        </div>
+      </div> */}
+      <h2>WORK IN PROGRESS!</h2>
+    </div>
+  );
+};
+
+export default Illustrations;
