8a54a34d7a4043fafba79dcd5cc4d501866a740b
Jady Hom <hyjady@gmail.com>
2021-07-20 17:07:21 -0400

    edited navbar - deleted other pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../styles/Navbar.css'
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="logo">{<Link to="/" className="name-logo">jady hom</Link>}</div>
-      <ul className="navbar-links">
-        <li className="nav-links">{<Link to="/works" className="nlinks">works</Link>}</li>
-        <li className="nav-links">{<Link to="/illustrations" className="nlinks">illustrations</Link>}</li>
-      </ul>
+      <div className="logo">
+        {
+          <Link to="/" className="name-logo">
+            jady hom
+          </Link>
+        }
+      </div>
     </nav>
   );
 };
