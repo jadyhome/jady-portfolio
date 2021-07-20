13089e29e165272ca525ca511b21ea6ca57de3ce
Jady Hom <hyjady@gmail.com>
2021-07-20 17:08:06 -0400

    removed unused pages

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import WorksPage from "../pages/WorksPage";
-import IllustrationsPage from "../pages/IllustrationsPage";
 
 const Router = () => {
   const [pageLoading, setPageLoading] = useState(true);
@@ -18,11 +16,6 @@ const Router = () => {
       ) : (
         <Switch>
           <Route exact path="/" component={() => <HomePage />} />
-          <Route path="/works" component={() => <WorksPage />} />
-          <Route
-            path="/illustrations"
-            component={() => <IllustrationsPage />}
-          />
         </Switch>
       )}
     </main>
