3cc508bf06a43c5e4fd2cb4959b39e10d8a50e0c
Jady Hom <hyjady@gmail.com>
2021-10-25 18:05:09 -0400

    used wildflowerdata for other images

--- src/wildflowerdata.js
+++ src/wildflowerdata.js
@@ -2,18 +2,216 @@ export const wildflowerdata = [
   {
     image: (
       <img
-        src="https://res.cloudinary.com/jadyhome/image/upload/v1634760630/portfolio/wildflower%20series/cover.jpg"
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197874/portfolio/wildflower%20series/wildflowers1.jpg"
         alt="wildflower-pic"
-        className="wildflower-image"
+        className="wildflower-image2"
       />
     ),
   },
   {
     image: (
       <img
-        src="https://res.cloudinary.com/jadyhome/image/upload/v1634760632/portfolio/wildflower%20series/titlepages.jpg"
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197874/portfolio/wildflower%20series/wildflowers2.jpg"
         alt="wildflower-pic"
-        className="wildflower-image"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197874/portfolio/wildflower%20series/wildflowers3.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197874/portfolio/wildflower%20series/wildflowers4.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197874/portfolio/wildflower%20series/wildflowers5.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197874/portfolio/wildflower%20series/wildflowers6.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers7.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers8.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers9.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers10.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers11.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers12.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers13.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers14.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers15.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers16.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers17.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers18.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers19.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197875/portfolio/wildflower%20series/wildflowers20.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers21.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers22.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers23.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1635197876/portfolio/wildflower%20series/wildflowers24.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image2"
       />
     ),
   },
