1933f65a6dbcc58142208f90925ed6c927eaef7a
Jady Hom <hyjady@gmail.com>
2021-10-25 14:57:06 -0400

    added botanydata file

---
+++ src/botanydata.js
@@ -0,0 +1,137 @@
+export const botanydata = [
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118604/portfolio/flower%20series/poinsettia_nz00et.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118604/portfolio/flower%20series/poppy_1_bcqbug.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118603/portfolio/flower%20series/pentas__lf6uzy.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118604/portfolio/flower%20series/tulip_ajuhvg.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118602/portfolio/flower%20series/azalea__yedfl1.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118603/portfolio/flower%20series/peony_1_dzrzow.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118603/portfolio/flower%20series/lisianthus_1_ninncq.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118602/portfolio/flower%20series/dahlia_1_lu506e.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118602/portfolio/flower%20series/bird_of_paradise_1_jfgluf.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118604/portfolio/flower%20series/sunflower_1_mnwa1t.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118602/portfolio/flower%20series/buttercup_1_qs8psh.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118604/portfolio/flower%20series/forsythia_cosxle.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118603/portfolio/flower%20series/neon_philodendron_sut0pe.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118603/portfolio/flower%20series/fiddle_leaf_fig_dkb0aa.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1633118603/portfolio/flower%20series/monstera_deliciosa_cshdba.jpg"
+        alt="flower-pic"
+        className="flower-image"
+      />
+    ),
+  },
+];
