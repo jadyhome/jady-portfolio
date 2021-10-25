60526939acecb77a4dfad13e9f616f0ff64e9bec
Jady Hom <hyjady@gmail.com>
2021-10-25 18:04:24 -0400

    renamed to wildflowerdataOne

---
+++ src/wildflowerdataOne.js
@@ -0,0 +1,20 @@
+export const wildflowerdataOne = [
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1634760630/portfolio/wildflower%20series/cover.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image"
+      />
+    ),
+  },
+  {
+    image: (
+      <img
+        src="https://res.cloudinary.com/jadyhome/image/upload/v1634760632/portfolio/wildflower%20series/titlepages.jpg"
+        alt="wildflower-pic"
+        className="wildflower-image"
+      />
+    ),
+  },
+];
